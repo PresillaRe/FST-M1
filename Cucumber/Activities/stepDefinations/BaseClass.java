86687bcd877014397b059b7fd5ea9aeaabc8aba5
PresillaRe <Presilla.Redkar@ibm.com>
2025-01-21 22:07:08 +0530

    Add files via upload

---
+++ Cucumber/Activities/stepDefinations/BaseClass.java
@@ -0,0 +1,13 @@
+package stepDefinations;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+//craete a new instance of the Firefox driver
+ static WebDriver driver;
+ static WebDriverWait wait;
+ 
+  }
+
