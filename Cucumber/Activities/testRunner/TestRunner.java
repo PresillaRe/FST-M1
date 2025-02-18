ed8a10a9b48e2f82dfa2c7e897ca984a82bcbc68
PresillaRe <Presilla.Redkar@ibm.com>
2025-02-18 18:43:25 +0530

    Add files via upload

--- Cucumber/Activities/testRunner/TestRunner.java
+++ Cucumber/Activities/testRunner/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.core.options.Constants;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("Features")
 @ConfigurationParameter(key= Constants.GLUE_PROPERTY_NAME,value="stepDefinations")
-@ConfigurationParameter(key= Constants.FILTER_TAGS_PROPERTY_NAME,value="@activity5")
+@ConfigurationParameter(key= Constants.FILTER_TAGS_PROPERTY_NAME,value="@activity6")
 @ConfigurationParameter(
 		key = Constants.PLUGIN_PROPERTY_NAME,
 		value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
